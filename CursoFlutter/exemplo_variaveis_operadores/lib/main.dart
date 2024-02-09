6cb8e883d9e03b61ff06d5f2fd5769fd7dddf40f
BarbozahLuis <luishbarboza0610@gmail.com>
2024-02-09 20:16:54 -0300

    09/02

---
+++ CursoFlutter/exemplo_variaveis_operadores/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // declarando e inicializando variaveis
+  String nome = "João";
+  int idade = 25;
+  double altura = 1.75;
+  bool estudante = true;
+
+  //imprimindo variaveis
+  print("Nome: $nome");
+  print("Nome: $idade anos");
+  print("Nome: $altura metros");
+  print("Nome: $estudante");
+}
+
+
