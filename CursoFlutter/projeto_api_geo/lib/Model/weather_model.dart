a5fa8fa2ec8a95b2093eca2e88beea332cd2b495
BarbozahLuis <luishbarboza0610@gmail.com>
2024-05-21 21:56:16 -0300

    21/05

--- CursoFlutter/projeto_api_geo/lib/Model/weather_model.dart
+++ CursoFlutter/projeto_api_geo/lib/Model/weather_model.dart
@@ -1,4 +1,30 @@
-class Weather{
+class Weather {
   //atributos
-  
+  final String name;
+  final String main;
+  final String description;
+  final double temp;
+  final double tempMax;
+  final double tempMin;
+
+  //construtor
+  Weather(
+      {required this.name,
+      required this.main,
+      required this.description,
+      required this.temp,
+      required this.tempMax,
+      required this.tempMin});
+
+      //fromJson
+      factory Weather.fromJson(Map<String, dynamic> json) {
+        return Weather(
+          name: json['name'],
+          main: json['weather'][0]['main'],
+          description: json['weather'][0]['description'],
+          temp: json['temp'],
+          tempMax: json['temp_max'],
+          tempMin: json['temp_min'],
+        );
+      }
 }
--- CursoFlutter/projeto_api_geo/lib/main.dart
+++ CursoFlutter/projeto_api_geo/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:projeto_api_geo/View/favorito_screen.dart';
-import 'package:projeto_api_geo/View/historic_screen.dart';
+
 import 'package:projeto_api_geo/View/home_screen.dart';
 
 void main(){
@@ -16,8 +15,7 @@ class MyApp extends StatelessWidget {
       home: HomeScreen(),
       debugShowCheckedModeBanner: false,
       routes:{
-        '/historico':(context) => HistoricoScreen(),
-        '/pesquisa':(context) => FavoritoScreen(),
+        
       } ,
     );
     
