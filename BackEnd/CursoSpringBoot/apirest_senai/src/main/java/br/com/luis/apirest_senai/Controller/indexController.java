b37ed6150122459fe7c6725451cf10434590df0f
BarbozahLuis <luishbarboza0610@gmail.com>
2024-06-13 21:25:03 -0300

    13/06

--- BackEnd/CursoSpringBoot/apirest_senai/src/main/java/br/com/luis/apirest_senai/Controller/indexController.java
+++ BackEnd/CursoSpringBoot/apirest_senai/src/main/java/br/com/luis/apirest_senai/Controller/indexController.java
@@ -3,8 +3,9 @@ package br.com.luis.apirest_senai.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class indexController {
-    @GetMapping("/login-responsavel")
-    public String acessoLoginResponsavel() {
+    @GetMapping("/")
+    public String acessoHome() {
         return "login/login-responsavel";
     }
+   
 }
